038a31f2b38baad77f7dc464916e2abd9ed9d4ad
Lori Jefitjian <lorijefitjiann@yahoo.com>
2022-04-09 15:32:51 -0700

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const drinks = require("./models/drinks.js")
 
 app.get('/', (req, res) => {
-    res.set("Welcome to the Gitpub App!")
+    res.send("Welcome to the Gitpub App!")
 })
 
-
-
+//app.get('/drinks/:id', (req, res) => {
+    //res.send(req.params.drinks)
+//})
+app.get("/drinks/", (req, res) => {
+    res.send({allDrinks: drinks});
+  });
 
 app.listen(PORT, () => {
     console.log("Listening to PORT...")
